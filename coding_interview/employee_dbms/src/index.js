532dcbc67ecd0a18d05ba9bf8b5963c330cbbbc0
123akhil <chaudharyakhil337@gmail.com>
2023-09-15 00:13:05 +0530

    basic of dbms ui and logic

--- coding_interview/employee_dbms/src/index.js
+++ coding_interview/employee_dbms/src/index.js
@@ -1,1 +1,60 @@
-console.log("hello world");
+(async function () {
+  const data = await fetch("./src/data.json");
+  const res = await data.json();
+
+  let employees = res;
+  let selectedEmployeeId = employees[0].id;
+  let selectedEmployee = employees[0];
+
+  const employeeList = document.querySelector(".employees__names--list");
+  const employeeInfo = document.querySelector(".employees__single--info");
+
+  //Add Employee Logic
+
+  //Select Employee Logic
+  employeeList.addEventListener("click", (e) => {
+    if (e.target.tagName === "SPAN" && selectedEmployeeId !== e.target.id) {
+      selectedEmployeeId = e.target.id;
+      renderEmployees();
+      renderSingleEmployee(); //Render SIngle EMployyee
+    }
+  });
+
+  const renderEmployees = () => {
+    //before rendering lets make clear that the div is clear out
+    employeeList.innerHTML = "";
+    employees.forEach((emp) => {
+      const employee = document.createElement("span");
+      employee.classList.add("employees__names--item");
+
+      //To identify or to add color to the selected employee make it a little darker.
+      if (parseInt(selectedEmployeeId, 10) === emp.id) {
+        employee.classList.add("selected");
+        selectedEmployee = emp;
+      }
+      employee.setAttribute("id", emp.id);
+      employee.innerHTML = `${emp.firstName} ${emp.lastName} <i class="employeeDelete">❌</i>`;
+
+      employeeList.append(employee);
+    });
+  };
+
+  //Render single Employee Info
+  const renderSingleEmployee = () => {
+    //deleting eployee logic
+
+    employeeInfo.innerHTML = `
+    <img src="${selectedEmployee.imageUrl}" />
+    <span class="employees__single--heading">
+    ${selectedEmployee.firstName} ${selectedEmployee.lastName} (${selectedEmployee.age})
+    </span>
+    <span>${selectedEmployee.address}</span>
+    <span>${selectedEmployee.email}</span>
+    <span>Mobile - ${selectedEmployee.contactNumber}</span>
+    <span>DOB - ${selectedEmployee.dob}</span>
+  `;
+  };
+
+  if (selectedEmployee) renderSingleEmployee();
+  renderEmployees();
+})();
